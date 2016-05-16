dfbbe2d02648e04b4adf6e6627d07344432e6544
k0Bas <emelianov.stanislav@gmail.com>
2016-05-16 21:29:25 +0300

    Add new contact test

---
+++ group_contact.py
@@ -0,0 +1,7 @@
+class GroupContact:
+
+    def __init__(self, firstname, lastname, title, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.title = title
+        self.email = email
---
+++ test_add_contact.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
+from group_contact import GroupContact
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_contact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_new_contact_page(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def create_contact(self, wd, group_contact):
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(group_contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(group_contact.lastname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(group_contact.title)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(group_contact.email)
+        # submit contact creation
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_new_contact_page(wd)
+        self.create_contact(wd, GroupContact(firstname="Stas", lastname="Emelianov", title="Mr", email="emelianov.stanislav@gmail.com"))
+        self.logout(wd)
+
+    def test_add_empty_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_new_contact_page(wd)
+        self.create_contact(wd, GroupContact(firstname="", lastname="", title="", email=""))
+        self.logout(wd)
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
